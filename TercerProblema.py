e7714b11b3707426db868896fcd86bf9d4ea90d3
Sebastian Hernandez <sebastian@Sebastians-MacBook-Air.local>
2022-08-31 22:35:50 -0500

    Primer Parcial

--- TercerProblema.py
+++ TercerProblema.py
@@ -27,5 +27,23 @@ def NR(a, i):
     else:
         return NR(xi, i)
 
+def Sec(a, b, i):
+    print("a")
+    xa=a
+    xac=b
+    xi=(-f(xac)*(-xac+xa))/(-f(xac)+f(xa)) + xac
+    i+=1
+    err = ea(xac, xi)
+    print("xi+1= ", xi)
+    print("Error Aproximado= ", err)
+    if(err<0.1):
+        return "Resuelto en ", i, " iteraciones"
+    else:
+        return Sec(xac, xi, i)
+
+print("Newton Raphson: ")
 print(NR(1, 0))
+i=0
+print("Metodo de Secante: ")
+print( Sec(0, 1, 0))
 
